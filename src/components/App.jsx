2aef3772746d47d1d2ec14722f24d3970990c2ac
Håkon Antonsen <haakon@antons1.net>
2019-01-21 22:31:23 +0100

    Make page look a little stylish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,7 +40,7 @@ class App extends Component {
 
     componentWillUnmount() {
         this.dbRef.off('child_added', this.firebaseDataUpdated);
-        //clearTimeout(this.fetcher);
+        //firebaseclearTimeout(this.fetcher);
     }
 
     firebaseDataUpdated(newData, prevData) {
@@ -89,21 +89,43 @@ export default App;
 
 const Station = ({ stationName, reachable, position, inside, outside }) =>
     <div>
-        <h3>{stationName} &#127969;</h3>
-        <Inside {...inside} />
+        <SensorTitle name={stationName} reachable={reachable} symbol="&#127969;" size={0} />
+        <Inside {...inside} reachable={reachable} />
         <Outside {...outside} />
     </div>;
 
-const Inside = ({ name, co2, humidity, noise, pressure, temperature }) =>
+const Inside = ({ name, co2, humidity, noise, pressure, temperature, reachable }) =>
     <div>
-        <h4>{name} &#128022;</h4>
-        Co2: {co2}<br />
-        Humidity: {humidity}
+        <SensorTitle name={name} reachable={reachable} symbol="&#128022;" size={1} />
+        <SensorData name="Temperature" value={temperature} unit="&#176;C" />
+        <SensorData name="Humidity" value={humidity} unit="%" />
+        <SensorData name="Noise" value={noise} unit="dB" />
+        <SensorData name="Pressure" value={pressure} unit="hpa" />
+        <SensorData name="CO2" value={co2} unit="ppm" />
     </div>;
 
-const Outside = ({ name, temperature, humidity, minTemperature, maxTemperature, minTemperatureTime, maxTemperatureTime }) =>
+const Outside = ({ name, temperature, humidity, minTemperature, maxTemperature, minTemperatureTime, maxTemperatureTime, reachable }) =>
     <div>
-        <h4>{name} &#128016;</h4>
-        Temperature: {temperature} ({minTemperature} - {maxTemperature})<br />
-        Humidity: {humidity}
+        <SensorTitle name={name} reachable={reachable} symbol="&#128016;" size={1} />
+        <SensorData name="Temperature" value={temperature} unit="&#176;C" />
+        <SensorData name="Humidity" value={humidity} unit="%" />
     </div>;
+
+const StatusLight = ({ reachable }) => <span className={`status-light ${reachable ? 'status-light--reachable' : 'status-light--unreachable'}`}></span>
+
+const SensorTitle = ({ name, reachable, symbol, size }) =>
+    <div className="sensor-title">
+        {size === 0 ? <h2 className="sensor-title__name">{name} {symbol}</h2> : null}
+        {size === 1 ? <h4 className="sensor-title__name">{name} {symbol}</h4> : null}
+        <StatusLight reachable={reachable} />
+    </div>
+
+const SensorData = ({ name, unit, value, level, trend}) =>
+    <div className="sensor-data">
+        <h5 className="sensor-data__name">{name}</h5>
+        <div className="sensor-data__value-wrapper">
+            <div className="sensor-data__value">{value}</div>
+            <div className="sensor-data__unit">{unit}</div>
+        </div>
+        {trend ? <div className="sensor-data__trend">{trend}</div> : null}
+    </div>
